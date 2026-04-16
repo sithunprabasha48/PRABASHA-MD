50e97db7e92e276cc3ad87fcc59a978552aeb0fc
sithunprabasha48 <sithunprabasha48@gmail.com>
2026-04-16 10:47:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "vrBC3RQC#XCB0P05w03tpN8DSKhlaGpD3sFxMFuM2xeEXOIYIdHc",
+SESSION_ID: process.env.SESSION_ID || "nv4TiQ4Z#bJT9DC4t_J_2piWjXhJQaIM9DMarPIjV0Gua5z3J4ts",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sithunprabasha48/PRABASHA-MD/blob/main/Imgee/IMG_20260415_011832_030.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 PRABASHA-MD Is Alive Now😍*",
 BOT_OWNER: '94704519149',  // Replace with the owner's phone number
